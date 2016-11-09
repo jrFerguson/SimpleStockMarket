2a86a7faaaebef1389dd880e3182aadaf36468d5
James Ferguson <james@jamesrferguson.co.uk>
2016-11-09 22:44:33 +0000

    Add  class with main method

---
+++ SimpleStockMarket/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStockMarket
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
